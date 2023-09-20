cdcba1060a337ded6b1d1da768355d8b721c2df2
Ammy572 <141164113+Ammy572@users.noreply.github.com>
2023-09-20 14:31:16 +0530

    Update code.py

--- 12.13.3.106/codes/code.py
+++ 12.13.3.106/codes/code.py
@@ -4,7 +4,7 @@ import math
 def equation(p):
     return p * math.comb(5, 2) * p**2 * (1 - p)**3 - 9 * p * math.comb(5, 3) * p**3 * (1 - p)**2
 
-# Calculate p directly
+# Calculate p
 a = math.comb(5, 2)
 b = math.comb(5, 3)
 
